9b55b93dfa4276ff6257843181ffd2200731efb5
Jun Mukai <mukai@jmuk.org>
2012-10-25 08:23:30 +0000

    lines -> tokens.

--- editor.js
+++ editor.js
@@ -14,51 +14,46 @@ function loadFile(filename, callback) {
 }
 
 function parseText(contents) {
-    var lines = contents.split('\n');
-    var result = [];    
-    for (var i = 0; i < lines.length; i++) {
-        var lineResult = [];
-        var line = lines[i];
-        var spaceRE = /^\s/;
-        var otherRE = /^\S+/;
-        var res = [spaceRE, otherRE];
-        while (line.length > 0) {
-            var noMatch = true;
-            for (var j = 0; j < res.length; j++) {
-                var matched = res[j].exec(line);
-                if (matched) {
-                    lineResult.push(matched[0]);
-                    line = line.substring(matched[0].length);
-                    noMatch = false;
-                    break;
-                }
-            }
-            if (noMatch) {
-                lineResult.push(line[0]);
-                line = line.substring(1);
+    var result = [];
+    var linebreakRE = /^[\n\r]/;
+    var spaceRE = /^\s/;
+    var otherRE = /^\S+/;
+    var res = [linebreakRE, spaceRE, otherRE];
+    while (contents.length > 0) {
+        var noMatch = true;
+        for (var j = 0; j < res.length; j++) {
+            var matched = res[j].exec(contents);
+            if (matched) {
+                result.push(matched[0]);
+                contents = contents.substring(matched[0].length);
+                noMatch = false;
+                break;
             }
         }
-        result.push(lineResult);
+        if (noMatch) {
+            result.push(contents[0]);
+            contents = contents.substring(1);
+        }
     }
     return result;
 }
 
 function onFileLoaded(contents) {
     var contentArea = $('content-area');
-    var data = parseText(contents);
-    for (var i = 0; i < data.length; i++) {
-        var lineElement = document.createElement('div');
-        var line = data[i];
-        for (var j = 0; j < line.length; j++) {
-            if (line[j] == ' ')
-                lineElement.appendChild(document.createTextNode(String.fromCharCode(0xA0)));
-            else
-                lineElement.appendChild(document.createTextNode(line[j]));
+    var tokens = parseText(contents);
+    for (var i = 0; i < tokens.length; i++) {
+        var token = tokens[i];
+        if (token == '\n') {
+            contentArea.appendChild(document.createElement('br'));
+        } else if (token == ' ') {
+            var span = document.createElement('span');
+            span.textContent = String.fromCharCode(0xA0);
+            contentArea.appendChild(span);
+        } else {
+            var span = document.createElement('span');
+            span.textContent = token;
+            contentArea.appendChild(span);
         }
-        // TODO: remove this hack and set the height for each line.
-        if (line.length == 0)
-            lineElement.appendChild(document.createTextNode(String.fromCharCode(0x200B)));
-        contentArea.appendChild(lineElement);
     }
 }
 
