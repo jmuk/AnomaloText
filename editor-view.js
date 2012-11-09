2482e2a888a31a8bde7459de60642237b5ccd042
Jun Mukai <mukai@jmuk.org>
2012-11-09 07:46:23 +0000

    tab -> 8 spaces

--- editor-view.js
+++ editor-view.js
@@ -27,58 +27,58 @@ EditorView.prototype.commands = {
 EditorView.prototype.onKeyDown = function(ev) {
     // see: https://github.com/jmuk/chrome-skk/blob/master/testpage/mock.js
     var keyMap = {
-	8:"Backspace", 9:"Tab", 13:"Enter", 27:"Esc", 32:" ", 33:"PageUp",
-	34:"PageDown", 35:"End", 36:"Home", 37:"Left", 38:"Up", 39:"Right",
-	40:"Down", 43:"+", 46:"Delete", 48:"0", 49:"1", 50:"2", 51:"3", 52:"4",
-	53:"5", 54:"6", 55:"7", 56:"8", 57:"9", 59:";", 61:"=", 65:"a", 66:"b",
-	67:"c", 68:"d", 69:"e", 70:"f", 71:"g", 72:"h", 73:"i", 74:"j", 75:"k",
-	76:"l", 77:"m", 78:"n", 79:"o", 80:"p", 81:"q", 82:"r", 83:"s", 84:"t",
-	85:"u", 86:"v", 87:"w", 88:"x", 89:"y", 90:"z", 96:"0", 97:"1", 98:"2",
-	99:"3", 100:"4", 101:"5", 102:"6", 103:"7", 104:"8", 105:"9", 106: "*",
-	107:"+", 109:"-", 110:".", 111: "/", 112:"HistoryBack", 113:"HistoryForward",
-	114:"BrowserRefresh", 115:"ChromeOSFullscreen", 116:"ChromeOSSwitchWindow",
-	117:"BrightnessDown", 118:"BrightnessUp", 119:"AudioVolumeMute",
-	120:"AudioVolumeDown", 121:"AudioVolumeUp", 186:";", 187:"=", 188:",",
-	189:"-", 190:".", 191:"/", 192:"`", 219:"[",  220:"\\", 221:"]", 222:"'"
+        8:"Backspace", 9:"Tab", 13:"Enter", 27:"Esc", 32:" ", 33:"PageUp",
+        34:"PageDown", 35:"End", 36:"Home", 37:"Left", 38:"Up", 39:"Right",
+        40:"Down", 43:"+", 46:"Delete", 48:"0", 49:"1", 50:"2", 51:"3", 52:"4",
+        53:"5", 54:"6", 55:"7", 56:"8", 57:"9", 59:";", 61:"=", 65:"a", 66:"b",
+        67:"c", 68:"d", 69:"e", 70:"f", 71:"g", 72:"h", 73:"i", 74:"j", 75:"k",
+        76:"l", 77:"m", 78:"n", 79:"o", 80:"p", 81:"q", 82:"r", 83:"s", 84:"t",
+        85:"u", 86:"v", 87:"w", 88:"x", 89:"y", 90:"z", 96:"0", 97:"1", 98:"2",
+        99:"3", 100:"4", 101:"5", 102:"6", 103:"7", 104:"8", 105:"9", 106: "*",
+        107:"+", 109:"-", 110:".", 111: "/", 112:"HistoryBack", 113:"HistoryForward",
+        114:"BrowserRefresh", 115:"ChromeOSFullscreen", 116:"ChromeOSSwitchWindow",
+        117:"BrightnessDown", 118:"BrightnessUp", 119:"AudioVolumeMute",
+        120:"AudioVolumeDown", 121:"AudioVolumeUp", 186:";", 187:"=", 188:",",
+        189:"-", 190:".", 191:"/", 192:"`", 219:"[",  220:"\\", 221:"]", 222:"'"
     };
     var shiftKeyMap = {
-	192:"~", 48:")", 49:"!", 50:"@", 51:"#", 52:"$", 53:"%", 54:"^", 55:"&",
-	56:"*", 57:"(", 109:"_", 61:"+", 219:"{", 221:"}", 220:"|", 59:":",
-	222:"\"", 187:"+", 188:"<", 189:"_", 190:">", 191:"?", 192: '~'
+        192:"~", 48:")", 49:"!", 50:"@", 51:"#", 52:"$", 53:"%", 54:"^", 55:"&",
+        56:"*", 57:"(", 109:"_", 61:"+", 219:"{", 221:"}", 220:"|", 59:":",
+        222:"\"", 187:"+", 188:"<", 189:"_", 190:">", 191:"?", 192: '~'
     };
 
     if (this.receiver.incomposition)
-	return true;
+        return true;
 
     var key = (ev.shiftKey) ?
         (shiftKeyMap[ev.keyCode] || keyMap[ev.keyCode]) : keyMap[ev.keyCode];
     var shiftKey = ev.shiftKey;
     if (ev.shiftKey && key && key.length == 1) {
         key = key.toUpperCase();
-	shiftKey = false;
+        shiftKey = false;
     }
 
     var consumed = false;
     if (key != null) {
-	var commandText = key;
-	if (shiftKey)
-	    commandText = 'S-' + commandText;
-	if (ev.altKey)
-	    commandText = 'M-' + commandText;
-	if (ev.ctrlKey)
-	    commandText = 'C-' + commandText;
+        var commandText = key;
+        if (shiftKey)
+            commandText = 'S-' + commandText;
+        if (ev.altKey)
+            commandText = 'M-' + commandText;
+        if (ev.ctrlKey)
+            commandText = 'C-' + commandText;
 
-	console.log(commandText);
-	if (commandText in this.commands) {
-	    var method = this.viewModel[this.commands[commandText]];
-	    if (method) {
-		method.bind(this.viewModel)();
-		consumed = true;
-	    }
-	} else if (commandText.length == 1) {
-	    this.viewModel.insertText(commandText);
-	    consumed = true;
-	}
+        console.log(commandText);
+        if (commandText in this.commands) {
+            var method = this.viewModel[this.commands[commandText]];
+            if (method) {
+                method.bind(this.viewModel)();
+                consumed = true;
+            }
+        } else if (commandText.length == 1) {
+            this.viewModel.insertText(commandText);
+            consumed = true;
+        }
     }
     this.receiver.textContent = '';
     this.updateCaretIndicator();
@@ -130,7 +130,7 @@ EditorView.prototype.createEventReceiver = function() {
         selection.removeAllRanges();
         selection.addRange(caretRange);
 
-	this.viewModel.moveToPosition(ev.pageX, ev.pageY);
+        this.viewModel.moveToPosition(ev.pageX, ev.pageY);
         this.updateCaretIndicator();
     }).bind(this);
     
